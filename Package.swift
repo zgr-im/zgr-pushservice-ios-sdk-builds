e0d15791f4e2d4d14a0fef278ac25c3d343eb5f7
Alex_Shubin <ashubin@zagruzka.com>
2023-03-13 10:08:49 +0300

    Release 1.6.3

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "ZGRImSDK",
-                      url: "https://github.com/zgr-im/zgr-pushservice-ios-sdk-builds/releases/download/1.6.2/ZGRImSDK.xcframework.zip",
-                      checksum: "0243465c569d289fc7dd0093a5f8fb1dde9123aea2f8ea0c75803c2cc8cbd1d0")
+                      url: "https://github.com/zgr-im/zgr-pushservice-ios-sdk-builds/releases/download/1.6.3/ZGRImSDK.xcframework.zip",
+                      checksum: "3c3fb737411ef72f29e3174d3d22c54befaead8a1f1c228a80a01a7bac4b5987")
     ]
 )
